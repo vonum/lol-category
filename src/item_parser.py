6f4bfdd22818e9373484206aaec3d4b10a4d26a2
vonum <milan.keca7@gmail.com>
2017-01-16 17:10:23 +0100

    add item stats to data

--- src/item_parser.py
+++ src/item_parser.py
@@ -1,4 +1,9 @@
-def parse_item(item):
+import json
+
+with open('../raw_data/item.json') as items_data:
+  items = json.load(items_data)['data']
+
+def parse_items_for_player(player):
   #0 fhp
   #1 fmp
   #2 php_regen
@@ -12,34 +17,48 @@ def parse_item(item):
   #10 pattack_speed
   #11 pcrit_chance
   #12 plife_steal
-  item_stats = item['stats']
+  player_items = [
+    player['stats']['item0'],
+    player['stats']['item1'],
+    player['stats']['item2'],
+    player['stats']['item3'],
+    player['stats']['item4'],
+    player['stats']['item5']
+  ]
   stats = [0] * 13
-  for stat in item_stats:
-    if stat == 'FlatHPPoolMod':
-      stats[0] += item_stats[stat]
-    elif stat == 'FlatMPPoolMod':
-      stats[1] += item_stats[stat]
-    elif stat == 'PercentHPRegenMod':
-      stats[2] += item_stats[stat]
-    elif stat == 'PercentMPRegenMod':
-      stats[3] += item_stats[stat]
-    elif stat == 'FlatArmorMod':
-      stats[4] += item_stats[stat]
-    elif stat == 'FlatSpellBlockMod':
-      stats[5] += item_stats[stat]
-    elif stat == 'FlatPhysicalDamageMod':
-      stats[6] += item_stats[stat]
-    elif stat == 'FlatMagicDamageMod':
-      stats[7] += item_stats[stat]
-    elif stat == 'FlatMovementSpeedMod':
-      stats[8] += item_stats[stat]
-    elif stat == 'PercentMovementSpeedMod':
-      stats[9] += item_stats[stat]
-    elif stat == 'PercentAttackSpeedMod':
-      stats[10] += item_stats[stat]
-    elif stat == 'PercentCritChanceMod':
-      stats[11] += item_stats[stat]
-    elif stat == 'PercentLifeStealMod':
-      stats[12] += item_stats[stat]
+
+  for player_item_id in player_items:
+    if str(player_item_id) in items:
+      item = items[str(player_item_id)]
+      item_stats = item['stats']
+
+      for stat in item_stats:
+        if stat == 'FlatHPPoolMod':
+          stats[0] += item_stats[stat]
+        elif stat == 'FlatMPPoolMod':
+          stats[1] += item_stats[stat]
+        elif stat == 'PercentHPRegenMod':
+          stats[2] += item_stats[stat] * 100
+        elif stat == 'PercentMPRegenMod':
+          stats[3] += item_stats[stat] * 100
+        elif stat == 'FlatArmorMod':
+          stats[4] += item_stats[stat]
+        elif stat == 'FlatSpellBlockMod':
+          stats[5] += item_stats[stat]
+        elif stat == 'FlatPhysicalDamageMod':
+          stats[6] += item_stats[stat]
+        elif stat == 'FlatMagicDamageMod':
+          stats[7] += item_stats[stat]
+        elif stat == 'FlatMovementSpeedMod':
+          stats[8] += item_stats[stat]
+        elif stat == 'PercentMovementSpeedMod':
+          stats[9] += item_stats[stat] * 100
+        elif stat == 'PercentAttackSpeedMod':
+          stats[10] += item_stats[stat] * 100
+        elif stat == 'PercentCritChanceMod':
+          stats[11] += item_stats[stat] * 100
+        elif stat == 'PercentLifeStealMod':
+          stats[12] += item_stats[stat] * 100
+
   return stats
 
--- src/transform_data.py
+++ src/transform_data.py
@@ -1,7 +1,7 @@
 import json
 import csv
 
-from item_parser import parse_item
+from item_parser import parse_items_for_player
 #from pprint import pprint
 
 with open('../raw_data/matches1.json') as matches_data:
@@ -11,47 +11,38 @@ with open('../formatted_data/player_stats.csv', 'wb') as csvfile:
   writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
   for match in matches['matches']:
     for player in match['participants']:
-      playerStats = []
-      playerStats.append(player['spell1Id'])
-      playerStats.append(player['spell2Id'])
-      playerStats.append(player['timeline']['creepsPerMinDeltas']['zeroToTen'])
-      playerStats.append(player['timeline']['creepsPerMinDeltas']['tenToTwenty'])
+      player_stats = []
+      player_stats.append(player['spell1Id'])
+      player_stats.append(player['spell2Id'])
+      player_stats.append(player['timeline']['creepsPerMinDeltas']['zeroToTen'])
+      player_stats.append(player['timeline']['creepsPerMinDeltas']['tenToTwenty'])
       #playerStats.append(player['timeline']['creepsPerMinDeltas']['twentyToThirty'])
       #playerStats.append(player['timeline']['creepsPerMinDeltas']['thirtyToEnd'])
-      playerStats.append(player['timeline']['xpPerMinDeltas']['zeroToTen'])
-      playerStats.append(player['timeline']['xpPerMinDeltas']['tenToTwenty'])
+      player_stats.append(player['timeline']['xpPerMinDeltas']['zeroToTen'])
+      player_stats.append(player['timeline']['xpPerMinDeltas']['tenToTwenty'])
       #playerStats.append(player['timeline']['xpPerMinDeltas']['twentyToThirty'])
       #playerStats.append(player['timeline']['xpPerMinDeltas']['thirtyToEnd'])
-      playerStats.append(player['timeline']['goldPerMinDeltas']['zeroToTen'])
-      playerStats.append(player['timeline']['goldPerMinDeltas']['tenToTwenty'])
+      player_stats.append(player['timeline']['goldPerMinDeltas']['zeroToTen'])
+      player_stats.append(player['timeline']['goldPerMinDeltas']['tenToTwenty'])
       #playerStats.append(player['timeline']['goldPerMinDeltas']['twentyToThirty'])
       #playerStats.append(player['timeline']['goldPerMinDeltas']['thirtyToEnd'])
-      playerStats.append(player['stats']['kills'])
-      playerStats.append(player['stats']['deaths'])
-      playerStats.append(player['stats']['assists'])
-      playerStats.append(player['stats']['totalDamageDealt'])
-      playerStats.append(player['stats']['totalDamageDealtToChampions'])
-      playerStats.append(player['stats']['totalDamageTaken'])
-      playerStats.append(player['stats']['largestCriticalStrike'])
-      playerStats.append(player['stats']['totalHeal'])
-      playerStats.append(player['stats']['minionsKilled'])
-      playerStats.append(player['stats']['neutralMinionsKilled'])
-      playerStats.append(player['stats']['goldEarned'])
-      playerStats.append(player['stats']['physicalDamageDealt'])
-      playerStats.append(player['stats']['magicDamageDealt'])
-      playerStats.append(player['stats']['trueDamageDealt'])
-      playerStats.append(player['stats']['wardsPlaced'])
-      playerStats.append(player['stats']['wardsKilled'])
-      writer.writerow(playerStats)
+      player_stats.append(player['stats']['kills'])
+      player_stats.append(player['stats']['deaths'])
+      player_stats.append(player['stats']['assists'])
+      player_stats.append(player['stats']['totalDamageDealt'])
+      player_stats.append(player['stats']['totalDamageDealtToChampions'])
+      player_stats.append(player['stats']['totalDamageTaken'])
+      player_stats.append(player['stats']['largestCriticalStrike'])
+      player_stats.append(player['stats']['totalHeal'])
+      player_stats.append(player['stats']['minionsKilled'])
+      player_stats.append(player['stats']['neutralMinionsKilled'])
+      player_stats.append(player['stats']['goldEarned'])
+      player_stats.append(player['stats']['physicalDamageDealt'])
+      player_stats.append(player['stats']['magicDamageDealt'])
+      player_stats.append(player['stats']['trueDamageDealt'])
+      player_stats.append(player['stats']['wardsPlaced'])
+      player_stats.append(player['stats']['wardsKilled'])
+      player_stats += parse_items_for_player(player)
+      writer.writerow(player_stats)
       #print playerStats
 
-with open('../raw_data/item.json') as items_data:
-  items = json.load(items_data)
-
-item = items['data']['1001']
-#print item['stats']
-parse_item(item)
-
-for item_id in items['data']:
-  print parse_item(items['data'][item_id])
-  print item_id
